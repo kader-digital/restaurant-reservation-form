1b8edc60a9cd10f8247dbff258f02f1f6acf4f4c
Lionel Penrose <lionelpenrose@Lionel-Penroses-MacBook.local>
2020-09-23 05:17:39 -0400

    initial commit for 1701.city (domain) ALMOST fully functional reservation form. I just need to make sure that after the closing hour the page resets to the next day, and that it only disables the select option elements during the pm

---
+++ set_current_time.js
@@ -0,0 +1,27 @@
+var date = new Date();
+
+var hour = date.getHours();
+var min = date.getMinutes();
+
+var time = hour + ":" + min;
+
+document.getElementById("current-time").innerHTML = time;
+
+var current_date = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate();
+document.getElementById("date").value = current_date;
+
+options = document.getElementsByClassName("res-time");
+
+for (var i = 0; i < options.length; i++) {
+
+	var hours_and_mins = options[i].value.split(':');
+	var res_hour = hours_and_mins[0];
+	var res_min = hours_and_mins[1];
+
+	if (parseInt(res_hour) <= parseInt(hour) && parseInt(res_min) <= parseInt(min))
+		options[i].disabled = true;
+
+
+} 
+
+
